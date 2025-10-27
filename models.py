0db802cfc7cd492108940d9edf80249b1e8464e9
Ryberxy <gagomartin2006@gmail.com>
2025-10-28 00:01:18 +0100

    Update models.py

--- models.py
+++ models.py
@@ -4,13 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class Usuario:
     def __init__(self, username, password, nombre, email, admin=False):
         self.username = username
-        self.password = generate_password_hash(password)  # hash seguro
+        self.password = generate_password_hash(password)
         self.nombre = nombre
         self.email = email
         self.admin = admin
 
     def to_dict(self):
-        """Convierte el objeto Usuario en diccionario para insertar en MongoDB"""
+        """Convierte el objeto Usuario en diccionario para insertarlo en MongoDB"""
         return {
             "username": self.username,
             "password": self.password,
@@ -21,5 +21,5 @@ class Usuario:
 
     @staticmethod
     def verificar_password(hash_guardado, password_introducida):
-        """Verifica que la contraseña introducida coincida con el hash"""
+        """Aqui se verifica que la contraseña introducida coincida con el hash"""
         return check_password_hash(hash_guardado, password_introducida)
